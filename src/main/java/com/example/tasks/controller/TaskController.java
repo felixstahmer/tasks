24c02bd530c07bd45b2ee60f4b8e587fccf1d5b3
Felix Stahmer <felix.stahmer@justin-legal.com>
2025-10-29 19:21:30 +0100

    Add Tasks Controller

---
+++ src/main/java/com/example/tasks/controller/TaskController.java
@@ -0,0 +1,31 @@
+package com.example.tasks.controller;
+
+import com.example.tasks.model.Task;
+import com.example.tasks.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/tasks")
+    public List<Task> getAllTasks() { return taskService.findAll(); }
+
+    @GetMapping("/tasks/{id}")
+    public Task getTaskById(@PathVariable long id) { return taskService.findById(id); }
+
+    @DeleteMapping("/tasks/{id}")
+    public void deleteTaskById(@PathVariable long id) { taskService.deleteTask(id); }
+
+    @PostMapping("/tasks")
+    public Task createTask(@RequestBody Task task) { return taskService.createTask(task); }
+
+    @PutMapping("/tasks/{id}")
+    public Task updateTask(@PathVariable long id, @RequestBody Task task) { return taskService.updateTask(id, task); }
+}
